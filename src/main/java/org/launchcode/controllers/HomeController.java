efad30129ab9b139c6c83a861213a7124c352afe
tennilleterry <terry31terry@yahoo.com>
2017-11-29 15:02:21 -0600

    Finished TODOs

--- src/main/java/org/launchcode/controllers/HomeController.java
+++ src/main/java/org/launchcode/controllers/HomeController.java
@@ -16,9 +16,14 @@ public class HomeController {
     public String index(Model model) {
 
         HashMap<String, String> actionChoices = new HashMap<>();
+
+        //Add key/value to hashmap
         actionChoices.put("search", "Search");
         actionChoices.put("list", "List");
 
+
+        //Add search and list to index page
+        //In index template use for loop to pull out value from hashmap
         model.addAttribute("actions", actionChoices);
 
         return "index";
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,13 +1,16 @@
 package org.launchcode.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * Created by LaunchCode
@@ -23,5 +26,27 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String searchResults(Model model,
+                                           @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<HashMap<String, String>> jobs;
+
+        if (Objects.equals(searchType, "all")){
+            jobs = JobData.findByValue(searchTerm);
+        }
+        else{
+
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("title", "All jobs with " + searchType + ": " + searchTerm);
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", ListController.columnChoices);
+
+
+        return "search";
+    }
+
 
 }
